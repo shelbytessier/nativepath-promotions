daaf1d0687dd1673e9b78d437c150fac260e303b
shelbytessier <shelbytessier@gmail.com>
2026-01-07 00:24:17 -0800

    Move Collagen lifestyle image focal point down in Offers Library
    Changed objectPosition from 'center 35%' to 'center 65%'
    
    Result:
    - Image focus moved down (shows lower portion of photo)
    - More of the Collagen jar/bottle now visible
    - Better product visibility in the 150px card area

--- app/offers/page.tsx
+++ app/offers/page.tsx
@@ -316,7 +316,7 @@ export default function OffersLibraryPage() {
                     width: '100%', 
                     height: '100%', 
                     objectFit: 'cover',
-                    objectPosition: 'center 35%'
+                    objectPosition: 'center 65%'
                   }}
                 />
               ) : (
