bdac6c000f6eda376795c46061ffce2c5ccfec88
shelbytessier <shelbytessier@gmail.com>
2026-01-07 00:23:06 -0800

    Adjust Collagen lifestyle image focal point in Offers Library
    Changed objectPosition from 'center' to 'center 35%'
    
    Result:
    - Image focus moved up
    - Collagen bottle now better centered in visible area
    - More product visibility in the card header

--- app/offers/page.tsx
+++ app/offers/page.tsx
@@ -316,7 +316,7 @@ export default function OffersLibraryPage() {
                     width: '100%', 
                     height: '100%', 
                     objectFit: 'cover',
-                    objectPosition: 'center'
+                    objectPosition: 'center 35%'
                   }}
                 />
               ) : (
